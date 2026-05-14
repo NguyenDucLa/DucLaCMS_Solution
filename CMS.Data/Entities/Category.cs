68fcb20385a25e2d4af24a4cba8a469f7660ec32
Admin <Admin@D20814>
2026-05-14 13:45:56 +0700

    Add project files.

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Họ và tên: Nguyễn Đức La
+ * Mssv: 2123110087
+ * ngày tạo: 14/05/2026
+ * version: 1.0
+ * 
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // Tên danh mục (vd: Tin Giáo Dục)
+        public string Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều bài viết
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
+
