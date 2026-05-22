0a25730fb5c0741df701b0a07022e7531d5cf7b8
NguyenDucLa <nguyenducla113@gmail.com>
2026-05-22 12:10:35 +0700

    Hoàn thành buổi 3 , và hiển thị ra trang chủ

--- CMS.Backend/Controllers/PostController.cs
+++ CMS.Backend/Controllers/PostController.cs
@@ -2,6 +2,7 @@
 using CMS.Data;
 using CMS.Data.Entities;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMS.Backend.Controllers
 {
@@ -15,11 +16,46 @@ namespace CMS.Backend.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? id)
         {
-            // Lấy tất cả bài viết từ Database
-            var posts = _context.Posts.ToList();
+            // 1. Kiểm tra nếu không có id truyền vào thì lấy toàn bộ bài viết
+            if (id == null)
+            {
+                var allPosts = _context.Posts
+                            .OrderByDescending(p => p.CreatedDate)
+                            .Include(p => p.Category)
+                            .ToList();
+                return View(allPosts);
+            }
+
+            // 2. Nếu có id thì lọc theo danh mục
+            var posts = _context.Posts
+                        .Where(p => p.CategoryId == id)
+                        .OrderByDescending(p => p.CreatedDate)
+                        .Include(p => p.Category)
+                        .ToList();
+
+            // 3. Truyền dữ liệu ra View
             return View(posts);
+        }
+
+        // GET: Post/Details/5
+        public IActionResult Details(int id)
+        {
+            // 1. Truy vấn bài viết theo ID
+            // Sử dụng .Include(p => p.Category) để lấy kèm thông tin Danh mục (Join bảng)
+            var post = _context.Posts
+                .Include(p => p.Category)
+                .FirstOrDefault(p => p.Id == id);
+
+            // 2. Kiểm tra nếu không tìm thấy bài viết (tránh lỗi màn hình trắng)
+            if (post == null)
+            {
+                return NotFound(); // Trả về trang lỗi 404
+            }
+
+            // 3. Truyền dữ liệu sang View
+            return View(post);
         }
     }
 }
